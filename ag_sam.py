1dc31f11b5e5074594a13ecde5f0d9f65a0b94a5
rxp190007 <rxp190007@utdallas.edu>
2025-04-09 23:33:15 -0500

    Segmentation for AG!

---
+++ ag_sam.py
@@ -0,0 +1,300 @@
+import os
+import pickle
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+from constants import Constants as const
+from sam2.build_sam import build_sam2_video_predictor
+
+class AgSam(Dataset):
+
+    def __init__(
+            self,
+            phase,
+            mode,
+            datasize,
+            data_path,
+            filter_nonperson_box_frame=True,
+            filter_small_box=False
+    ):
+
+        root_path = data_path
+        self._phase = phase
+        self._mode = mode
+        self._datasize = datasize
+        self._data_path = data_path
+        self._frames_path = os.path.join(root_path, "frames")
+
+        self._fetch_object_classes()
+
+        # Fetch object and person bounding boxes
+        person_bbox, object_bbox = self._fetch_object_person_bboxes(self._datasize, filter_small_box)
+
+        # collect valid frames
+        video_dict, q = self._fetch_valid_frames(person_bbox, object_bbox)
+        all_video_names = np.unique(q)
+
+        # Build dataset
+        self._build_dataset(video_dict, person_bbox, object_bbox, all_video_names, filter_nonperson_box_frame)
+
+        # Initialize SAMv2 predictor
+        sam2_checkpoint = os.path.join(os.getcwd(), "checkpoints/sam2.1_hiera_large.pt")
+        # model_cfg = os.path.join(os.getcwd(), "sam2/configs/sam2.1/sam2.1_hiera_l.yaml")
+        model_cfg = "sam2.1_hiera_l"
+
+        self._device = torch.device("cuda")
+        self._sam_predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=self._device)
+
+    @staticmethod
+    def show_mask(mask, ax, obj_id=None, random_color=False):
+        if random_color:
+            color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+        else:
+            cmap = plt.get_cmap("tab10")
+            cmap_idx = 0 if obj_id is None else obj_id
+            color = np.array([*cmap(cmap_idx)[:3], 0.6])
+        h, w = mask.shape[-2:]
+        mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+        ax.imshow(mask_image)
+
+    @staticmethod
+    def show_points(coords, labels, ax, marker_size=200):
+        pos_points = coords[labels == 1]
+        neg_points = coords[labels == 0]
+        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white',
+                   linewidth=1.25)
+        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white',
+                   linewidth=1.25)
+
+    @staticmethod
+    def show_box(box, ax):
+        x0, y0 = box[0], box[1]
+        w, h = box[2] - box[0], box[3] - box[1]
+        ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+
+    def _fetch_object_classes(self):
+        self.object_classes = [const.BACKGROUND]
+        with open(os.path.join(self._data_path, const.ANNOTATIONS, const.OBJECT_CLASSES_FILE), 'r',
+                  encoding='utf-8') as f:
+            for line in f.readlines():
+                line = line.strip('\n')
+                self.object_classes.append(line)
+        f.close()
+        self.object_classes[9] = 'closet/cabinet'
+        self.object_classes[11] = 'cup/glass/bottle'
+        self.object_classes[23] = 'paper/notebook'
+        self.object_classes[24] = 'phone/camera'
+        self.object_classes[31] = 'sofa/couch'
+
+    def _fetch_object_person_bboxes(self, datasize, filter_small_box=False):
+        annotations_path = os.path.join(self._data_path, const.ANNOTATIONS)
+        if filter_small_box:
+            with open(os.path.join(annotations_path, const.PERSON_BOUNDING_BOX_PKL), 'rb') as f:
+                person_bbox = pickle.load(f)
+            f.close()
+            with open(os.path.join(annotations_path, const.OBJECT_BOUNDING_BOX_RELATIONSHIP_FILTERSMALL_PKL), 'rb') as f:
+                object_bbox = pickle.load(f)
+        else:
+            with open(os.path.join(annotations_path, const.PERSON_BOUNDING_BOX_PKL), 'rb') as f:
+                person_bbox = pickle.load(f)
+            f.close()
+            with open(os.path.join(annotations_path, const.OBJECT_BOUNDING_BOX_RELATIONSHIP_PKL), 'rb') as f:
+                object_bbox = pickle.load(f)
+            f.close()
+        return person_bbox, object_bbox
+
+    def _fetch_valid_frames(self, person_bbox, object_bbox):
+        video_dict = {}
+        q = []
+        for i in person_bbox.keys():
+            if object_bbox[i][0][const.METADATA][const.SET] == self._phase:  # train or testing?
+                video_name, frame_num = i.split('/')
+                q.append(video_name)
+                frame_valid = False
+                for j in object_bbox[i]:  # the frame is valid if there is visible bbox
+                    if j[const.VISIBLE]:
+                        frame_valid = True
+                if frame_valid:
+                    video_name, frame_num = i.split('/')
+                    if video_name in video_dict.keys():
+                        video_dict[video_name].append(i)
+                    else:
+                        video_dict[video_name] = [i]
+        return video_dict, q
+
+    def _build_dataset(self, video_dict, person_bbox, object_bbox, all_video_names, filter_nonperson_box_frame=True):
+        self._valid_video_names = []
+        self._video_list = []
+        self._video_size = []  # (w,h)
+        self._gt_annotations = []
+        self._non_gt_human_nums = 0
+        self._non_heatmap_nums = 0
+        self._non_person_video = 0
+        self._one_frame_video = 0
+        self._valid_nums = 0
+        self._invalid_videos = []
+
+        '''
+        filter_nonperson_box_frame = True (default): according to the stanford method, remove the frames without person box both for training and testing
+        filter_nonperson_box_frame = False: still use the frames without person box, FasterRCNN may find the person
+        '''
+        for i in video_dict.keys():
+            video = []
+            gt_annotation_video = []
+            for j in video_dict[i]:
+                if filter_nonperson_box_frame:
+                    if person_bbox[j][const.BOUNDING_BOX].shape[0] == 0:
+                        self._non_gt_human_nums += 1
+                        continue
+                    else:
+                        video.append(j)
+                        self._valid_nums += 1
+
+                gt_annotation_frame = [
+                    {
+                        const.PERSON_BOUNDING_BOX: person_bbox[j][const.BOUNDING_BOX],
+                        const.FRAME: j
+                    }
+                ]
+
+                # each frame's objects and human
+                for k in object_bbox[j]:
+                    if k[const.VISIBLE]:
+                        assert k[const.BOUNDING_BOX] is not None, 'warning! The object is visible without bbox'
+                        k[const.CLASS] = self.object_classes.index(k[const.CLASS])
+                        # from xywh to xyxy
+                        k[const.BOUNDING_BOX] = np.array([
+                            k[const.BOUNDING_BOX][0], k[const.BOUNDING_BOX][1],
+                            k[const.BOUNDING_BOX][0] + k[const.BOUNDING_BOX][2],
+                            k[const.BOUNDING_BOX][1] + k[const.BOUNDING_BOX][3]
+                        ])
+                        gt_annotation_frame.append(k)
+                gt_annotation_video.append(gt_annotation_frame)
+
+            if len(video) > 2:
+                self._video_list.append(video)
+                self._video_size.append(person_bbox[j][const.BOUNDING_BOX_SIZE])
+                self._gt_annotations.append(gt_annotation_video)
+            elif len(video) == 1:
+                self._one_frame_video += 1
+            else:
+                self._non_person_video += 1
+
+        print('x' * 60)
+        if filter_nonperson_box_frame:
+            print('There are {} videos and {} valid frames'.format(len(self._video_list), self._valid_nums))
+            print('{} videos are invalid (no person), remove them'.format(self._non_person_video))
+            print('{} videos are invalid (only one frame), remove them'.format(self._one_frame_video))
+            print('{} frames have no human bbox in GT, remove them!'.format(self._non_gt_human_nums))
+        else:
+            print('There are {} videos and {} valid frames'.format(len(self._video_list), self._valid_nums))
+            print('{} frames have no human bbox in GT'.format(self._non_gt_human_nums))
+            print('Removed {} of them without joint heatmaps which means FasterRCNN also cannot find the human'.format(
+                self._non_heatmap_nums))
+        print('x' * 60)
+
+        self.invalid_video_names = np.setdiff1d(all_video_names, self._valid_video_names, assume_unique=False)
+
+
+    def __len__(self):
+        return len(self._video_list)
+
+    def __getitem__(self, index):
+        frame_names = self._video_list[index]
+        gt_annotation_frame = self._gt_annotations[index]
+
+        # 1. Create a map of object-id and the first frame it appears in the video.
+        video_id = frame_names[0].split('/')[0]
+        object_id_map = {}
+        for i, frame_details_dict in enumerate(gt_annotation_frame):
+            # Add the person bbox information from the first frame
+            if i == 0:
+                person_bbox = frame_details_dict[0][const.PERSON_BOUNDING_BOX]
+                object_id_map[-1] = {}
+                object_id_map[-1]['frame'] = frame_details_dict[0][const.FRAME].split('/')[-1]
+                object_id_map[-1]['bbox'] = person_bbox
+            frame_number = frame_details_dict[0][const.FRAME].split('/')[-1]
+            for obj_details_dict in frame_details_dict[1:]:
+                obj_id = obj_details_dict[const.CLASS]
+                if obj_id not in object_id_map:
+                    object_id_map[obj_id] = {}
+                    object_id_map[obj_id]['frame'] = frame_number
+                    object_id_map[obj_id]['bbox'] = obj_details_dict[const.BOUNDING_BOX]
+
+        # 2. Sort the object-id map based on the frame number
+        object_id_map = dict(sorted(object_id_map.items(), key=lambda item: item[1]['frame']))
+
+        # 3. Use SAMv2 for each object individually and get the mask starting from the first frame it appears in the video using the bbox provided.
+        # a. Prepare the input for SAMv2
+        video_dir = os.path.join(self._frames_path, video_id)
+        inference_state = self._sam_predictor.init_state(video_path=video_dir)
+
+        # b. Run SAMv2 for each object
+        for obj_id, obj_details in object_id_map.items():
+            # Reset the inference state for each object
+            self._sam_predictor.reset_state(inference_state)
+            frame_number = obj_details['frame']
+            class_id = obj_id
+
+            frame_path = os.path.join(video_dir, frame_number)
+
+
+        return frame_names
+
+
+def cuda_collate_fn(batch):
+    """
+    don't need to zip the tensor
+
+    """
+    return batch[0]
+
+
+def main(mode):
+    train_dataset = AgSam(
+        phase="train",
+        mode=mode,
+        datasize="large",
+        data_path="/data/rohith/ag/",
+        filter_nonperson_box_frame=True,
+        filter_small_box=False if mode == 'predcls' else True
+    )
+
+    test_dataset = AgSam(
+        phase="test",
+        mode=mode,
+        datasize="large",
+        data_path="/data/rohith/ag/",
+        filter_nonperson_box_frame=True,
+        filter_small_box=False if mode == 'predcls' else True
+    )
+
+    dataloader_train = DataLoader(
+        train_dataset,
+        shuffle=True,
+        collate_fn=cuda_collate_fn,
+        pin_memory=True,
+        num_workers=0
+    )
+
+    dataloader_test = DataLoader(
+        test_dataset,
+        shuffle=False,
+        collate_fn=cuda_collate_fn,
+        pin_memory=False
+    )
+
+    object_classes = train_dataset.object_classes
+
+
+    for i, data in enumerate(dataloader_train):
+        print(data)
+        break
+
+
+if __name__ == '__main__':
+    main(const.PREDCLS)
+    main(const.SGCLS)
+
---
+++ constants.py
@@ -0,0 +1,126 @@
+class Constants:
+    DISTRIBUTION = "distribution"
+    INDICES = "indices"
+    LABELS = "labels"
+    FRAME = "frame"
+    FRAMES = "frames"
+    CLASS = "class"
+    BACKGROUND = "__background__"
+    ANNOTATIONS = "annotations"
+
+    MINI = "mini"
+    METADATA = "metadata"
+    SET = "set"
+    VISIBLE = "visible"
+    BOUNDING_BOX = "bbox"
+    UNION_BOX = "union_box"
+    PERSON_BOUNDING_BOX = "person_bbox"
+    BOUNDING_BOX_SIZE = "bbox_size"
+    STATE_DICT = "state_dict"
+    FEATURES = "features"
+    PAIR_IDX = "pair_idx"
+    IM_IDX = "im_idx"
+    HUMAN_IDX = "human_idx"
+    IM_INFO = "im_info"
+    UNION_FEAT = "union_feat"
+    SPATIAL_MASKS = "spatial_masks"
+    PRED_LABELS = "pred_labels"
+    BOXES = "boxes"
+    FMAPS = "fmaps"
+    PRED_SCORES = "pred_scores"
+    ATTENTION_DISTRIBUTION = "attention_distribution"
+    SPATIAL_DISTRIBUTION = "spatial_distribution"
+    CONTACTING_DISTRIBUTION = "contacting_distribution"
+    ADAM = "adam"
+    ADAMW = "adamw"
+    SGD = "sgd"
+
+    STL_LOSS = "stl_loss"
+    OBJECT_LOSS = "object_loss"
+    FRAMES_PREDCLS = "frames_predcls"
+    FRAMES_SGCLS = "frames_sgcls"
+    FRAMES_SGDET = "frames_sgdet"
+
+    GLOVE_6B = "glove.6B"
+
+    SGDET = "sgdet"
+    SGCLS = "sgcls"
+    PREDCLS = "predcls"
+
+    TRAIN = "train"
+    TEST = "test"
+
+    FRAME_IDX = "frame_idx"
+
+    OBJECT_CLASSES_FILE = "object_classes.txt"
+    RELATIONSHIP_CLASSES_FILE = "relationship_classes.txt"
+    PERSON_BOUNDING_BOX_PKL = "person_bbox.pkl"
+    OBJECT_BOUNDING_BOX_RELATIONSHIP_PKL = "object_bbox_and_relationship.pkl"
+    PROGRESS_TEXT_FILE = "progress.txt"
+    OBJECT_BOUNDING_BOX_RELATIONSHIP_FILTERSMALL_PKL = "object_bbox_and_relationship_filtersmall.pkl"
+
+    ATTENTION_RELATIONSHIP = "attention_relationship"
+    SPATIAL_RELATIONSHIP = "spatial_relationship"
+    CONTACTING_RELATIONSHIP = "contacting_relationship"
+
+    ATTENTION_GT = "attention_gt"
+    SPATIAL_GT = "spatial_gt"
+    CONTACTING_GT = "contacting_gt"
+
+    ATTENTION_GT_MASK = "attention_gt_mask"
+    SPATIAL_GT_MASK = "spatial_gt_mask"
+    CONTACTING_GT_MASK = "contacting_gt_mask"
+
+    GLOBAL_OUTPUT = "global_output"
+
+    ATTENTION_RELATION_LOSS = "attention_relation_loss"
+    SPATIAL_RELATION_LOSS = "spatial_relation_loss"
+    CONTACTING_RELATION_LOSS = "contacting_relation_loss"
+
+    CUDA_DEVICE = 0
+    NMS_THRESHOLD = 0.4
+    SCORE_THRESHOLD = 0.1
+    FASTER_RCNN_BATCH_SIZE = 10
+
+    FINAL_BBOXES = "FINAL_BBOXES"
+    FINAL_SCORES = "FINAL_SCORES"
+    FINAL_LABELS = "FINAL_LABELS"
+    IMAGE_IDX = "im_idx"
+    PAIR_IDX = "pair_idx"
+    HUMAN_IDX = "human_idx"
+    FINAL_FEATURES = "FINAL_FEATURES"
+    UNION_FEATURES = "union_features"
+    UNION_BOX = "union_box"
+    SPATIAL_MASKS = "spatial_masks"
+    SCORES = "scores"
+
+    ATTENTION_REL = "a_rel"
+    SPATIAL_REL = "s_rel"
+    CONTACTING_REL = "c_rel"
+
+    FINAL_DISTRIBUTIONS = "FINAL_DISTRIBUTIONS"
+    FINAL_BASE_FEATURES = "FINAL_BASE_FEATURES"
+
+    DETECTOR_FOUND_IDX = "DETECTOR_FOUND_IDX"
+    GT_RELATIONS = "GT_RELATIONS"
+    SUPPLY_RELATIONS = "SUPPLY_RELATIONS"
+    ASSIGNED_LABELS = "ASSIGNED_LABELS"
+
+    FINAL_BBOXES_X = "FINAL_BBOXES_X"
+    FINAL_LABELS_X = "FINAL_LABELS_X"
+    FINAL_SCORES_X = "FINAL_SCORES_X"
+    FINAL_FEATURES_X = "FINAL_FEATURES_X"
+    PAIR = "pair"
+
+    GT_ANNOTATION = "gt_annotation"
+    FRAME_SIZE = "frame_size"
+
+    PERSON = "person"
+    OBJECT = "object"
+
+    PARTIAL_PERCENTAGE = "partial_percentage"
+    PARTIAL = "partial"
+    LABEL_NOISE = "label_noise"
+
+    SGG = "sgg"
+    SGA = "sga"
